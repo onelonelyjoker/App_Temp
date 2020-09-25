83fef4ff61b66f223f6899d02bed8a00aa0e4ebc
ghostwarriorvoidmain <kumar.jeetu04@gmail.com>
2020-09-25 15:15:23 +0530

    nice

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
+
+import * as React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import LoginForm from "./src/components/LoginForm";
-import { Provider as PaperProvider } from "react-native-paper";
-export default function App() {
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+
+
+
+function HomeScreen({navigation}) {
+  setTimeout(()=> {
+    navigation.replace('Login');
+  }, 2000);
   return (
-    <PaperProvider>
-      <LoginForm />
-    </PaperProvider>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+//This is HomeScreen Component.
+
+const Stack = createStackNavigator();
+//This is Navigator 
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home" headerMode="none">
+        <Stack.Screen name = "Home" component = {HomeScreen}/>
+        <Stack.Screen name = "Login" component = {LoginForm}/>
+        
+      </Stack.Navigator>    
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+
+export default App;
+
